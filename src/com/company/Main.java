6eca415fe2b158a2e4f8cfe482dad6345da52fe9
Ralph F <RaftsDev@gmail.com>
2020-10-19 22:16:52 -0400

    Feat: Populate sets with squares and cubes.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Set<Integer> squares = new HashSet<>();
+        Set<Integer> cubes = new HashSet<>();
+
+        for(int i=1; i<=100; i++){
+            squares.add(i * i);
+            cubes.add(i * i * i);
+        }
+
+        System.out.println("Square size: " + squares.size() + " cubes size: " + cubes.size());
+
     }
 }
